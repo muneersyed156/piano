239b6135e53f8e9d6469afaa6f3acffa40e5606e
muneersyed156 <muneersyed15@yahoo.in>
2019-09-10 01:32:36 +0530

    First commit

---
+++ piano.js
@@ -0,0 +1,367 @@
+var i = document.getElementById('txt')
+//keys DOM
+var ah3 = document.getElementById("ah3")
+var fh3 = document.getElementById("fh3")
+var fh4 = document.getElementById("fh4")
+var fh5 = document.getElementById("fh5")
+var g5 = document.getElementById("g5")
+var g3 = document.getElementById("g3")
+var g4 = document.getElementById("g4")
+var gh4 = document.getElementById("gh4")
+var gh5 = document.getElementById("gh5")
+var f5 = document.getElementById("f5")
+var gh3 = document.getElementById("gh3")
+var dh5 = document.getElementById("dh5")
+var d3 = document.getElementById("d3")
+var c5 = document.getElementById("c5")
+var c6 = document.getElementById("c6")
+var dh3 = document.getElementById("dh3")
+var dh4 = document.getElementById("dh4")
+var ch4 = document.getElementById("ch4")
+var ch5 = document.getElementById("ch5")
+var c3 = document.getElementById("c3")
+var c4 = document.getElementById("c4")
+var e5 = document.getElementById("e5")
+var d5 = document.getElementById("d5")
+var d4 = document.getElementById("d4")
+var e4 = document.getElementById("e4")
+var e3 = document.getElementById("e3")
+var f3 = document.getElementById("f3")
+var b5 = document.getElementById("b5")
+var ch3 = document.getElementById("ch3")
+var a4 = document.getElementById("a4")
+var a5 = document.getElementById("a5")
+var b3 = document.getElementById("b3")
+var b4 = document.getElementById("b4")
+var ah4 = document.getElementById("ah4")
+var a5 = document.getElementById("a5")
+var a3 = document.getElementById("a3")
+//MUSIC'S DOM
+var ah3m = document.getElementById("ah3m")
+var fh3m = document.getElementById("fh3m")
+var fh4m = document.getElementById("fh4m")
+var fh5m = document.getElementById("fh5m")
+var g5m = document.getElementById("g5m")
+var g3m = document.getElementById("g3m")
+var g4m = document.getElementById("g4m")
+var gh4m = document.getElementById("gh4m")
+var gh5m = document.getElementById("gh5m")
+var f5m = document.getElementById("f5m")
+var gh3m = document.getElementById("gh3m")
+var dh5m = document.getElementById("dh5m")
+var d3m = document.getElementById("d3m")
+var c5m = document.getElementById("c5m")
+var c6m = document.getElementById("c6m")
+var dh3m = document.getElementById("dh3m")
+var dh4m = document.getElementById("dh4m")
+var ch4m = document.getElementById("ch4m")
+var ch5m = document.getElementById("ch5m")
+var c3m = document.getElementById("c3m")
+var c4m = document.getElementById("c4m")
+var e5m = document.getElementById("e5m")
+var d5m = document.getElementById("d5m")
+var d4m = document.getElementById("d4m")
+var e4m = document.getElementById("e4m")
+var e3m = document.getElementById("e3m")
+var f3m = document.getElementById("f3m")
+var b5m = document.getElementById("b5m")
+var ch3m = document.getElementById("ch3m")
+var a4m = document.getElementById("a4m")
+var a5m = document.getElementById("a5m")
+var b3m = document.getElementById("b3m")
+var b4m = document.getElementById("b4m")
+var ah4m = document.getElementById("ah4m")
+var a5m = document.getElementById("a5m")
+var a3m = document.getElementById("a3m")
+ah3.addEventListener("click", function () {
+    ah3m.play()
+    i.value = 'A#3'
+})
+fh3.addEventListener("click", function () {
+    fh3m.play()
+    i.value = 'F#3'
+})
+fh4.addEventListener("click", function () {
+    fh4m.play()
+    i.value = 'F#4'
+})
+fh5.addEventListener("click", function () {
+    fh5m.play()
+    i.value = 'F#5'
+})
+g5.addEventListener("click", function () {
+    g5m.play()
+    i.value = 'G#5'
+})
+g3.addEventListener("click", function () {
+    g3m.play()
+    i.value = 'G3'
+})
+g4.addEventListener("click", function () {
+    g4m.play()
+    i.value = 'G4'
+})
+gh4.addEventListener("click", function () {
+    gh4m.play()
+    i.value = 'G#4'
+})
+gh5.addEventListener("click", function () {
+    gh5m.play()
+    i.value = 'G#5'
+})
+f5.addEventListener("click", function () {
+    f5m.play()
+    i.value = 'F5'
+})
+gh3.addEventListener("click", function () {
+    gh3m.play()
+    i.value = 'G#3'
+})
+dh5.addEventListener("click", function () {
+    dh5m.play()
+    i.value = 'D#5'
+})
+d3.addEventListener("click", function () {
+    d3m.play()
+    i.value = 'D3'
+})
+c5.addEventListener("click", function () {
+    c5m.play()
+    i.value = 'C5'
+})
+c6.addEventListener("click", function () {
+    c6m.play()
+    i.value = 'C6'
+})
+dh3.addEventListener("click", function () {
+    dh3m.play()
+    i.value = 'D#3'
+})
+dh4.addEventListener("click", function () {
+    dh4m.play()
+    i.value = 'D#4'
+})
+ch4.addEventListener("click", function () {
+    ch4m.play()
+    i.value = 'C#4'
+})
+ch5.addEventListener("click", function () {
+    ch5m.play()
+    i.value = 'C#5'
+})
+c3.addEventListener("click", function () {
+    c3m.play()
+    i.value = 'C3'
+})
+c4.addEventListener("click", function () {
+    c4m.play()
+    i.value = 'C4'
+})
+e5.addEventListener("click", function () {
+    e5m.play()
+    i.value = 'E5'
+})
+d5.addEventListener("click", function () {
+    d5m.play()
+    i.value = 'D5'
+})
+d4.addEventListener("click", function () {
+    d4m.play()
+    i.value = 'D4'
+})
+e4.addEventListener("click", function () {
+    e4m.play()
+    i.value = 'E4'
+})
+e3.addEventListener("click", function () {
+    e3m.play()
+    i.value = 'E3'
+})
+f3.addEventListener("click", function () {
+    f3m.play()
+    i.value = 'F3'
+})
+b5.addEventListener("click", function () {
+    b5m.play()
+    i.value = 'B5'
+})
+ch3.addEventListener("click", function () {
+    ch3m.play()
+    i.value = 'C#3'
+})
+a4.addEventListener("click", function () {
+    a4m.play()
+    i.value = 'A4'
+})
+a5.addEventListener("click", function () {
+    a5m.play()
+    i.value = 'A5'
+})
+b3.addEventListener("click", function () {
+    b3m.play()
+    i.value = 'B3'
+})
+b4.addEventListener("click", function () {
+    b4m.play()
+    i.value = 'B4'
+})
+ah4.addEventListener("click", function () {
+    ah4m.play()
+    i.value = 'A#4'
+})
+ah5.addEventListener("click", function () {
+    ah5m.play()
+    i.value = 'A#5'
+})
+a3.addEventListener("click", function () {
+    a3m.play()
+    i.value = 'A3'
+})
+document.body.addEventListener('keydown', function (e) {
+    if (e.keyCode == 65) {
+        ah3m.play()
+        i.value = 'A#3'
+    }
+    if (e.keyCode == 83) {
+        fh3m.play()
+        i.value = 'F#3'
+    }
+    if (e.keyCode == 68) {
+        fh4m.play()
+        i.value = 'F#4'
+    }
+    if (e.keyCode == 70) {
+        fh5m.play()
+        i.value = 'F#5'
+    }
+    if (e.keyCode == 71) {
+        g5m.play()
+        i.value = 'G5'
+    }
+    if (e.keyCode == 72) {
+        g3m.play()
+        i.value = 'G3'
+    }
+    if (e.keyCode == 74) {
+        g4m.play()
+        i.value = 'G4'
+    }
+    if (e.keyCode == 75) {
+        gh4m.play()
+        i.value = 'G#4'
+    }
+    if (e.keyCode == 90) {
+        gh5m.play()
+        i.value = 'G#5'
+    }
+    if (e.keyCode == 88) {
+        f5m.play()
+        i.value = 'F#5'
+    }
+    if (e.keyCode == 67) {
+        gh3m.play()
+        i.value = 'G#3'
+    }
+    if (e.keyCode == 86) {
+        dh5m.play()
+        i.value = 'D#5'
+    }
+    if (e.keyCode == 66) {
+        d3m.play()
+        i.value = 'D3'
+    }
+    if (e.keyCode == 78) {
+        c5m.play()
+        i.value = 'C5'
+    }
+    if (e.keyCode == 77) {
+        c6m.play()
+        i.value = 'C6'
+    }
+    if (e.keyCode == 81) {
+        dh3m.play()
+        i.value = 'D#3'
+    }
+    if (e.keyCode == 87) {
+        dh4m.play()
+        i.value = 'D#4'
+    }
+    if (e.keyCode == 69) {
+        ch4m.play()
+        i.value = 'C#4'
+    }
+    if (e.keyCode == 82) {
+        ch5m.play()
+        i.value = 'C#5'
+    }
+    if (e.keyCode == 84) {
+        c3m.play()
+        i.value = 'C3'
+    }
+    if (e.keyCode == 89) {
+        c4m.play()
+        i.value = 'C4'
+    }
+    if (e.keyCode == 85) {
+        e5m.play()
+        i.value = 'E5'
+    }
+    if (e.keyCode == 73) {
+        d5m.play()
+        i.value = 'D5'
+    }
+    if (e.keyCode == 79) {
+        d4m.play()
+        i.value = 'D4'
+    }
+    if (e.keyCode == 80) {
+        e4m.play()
+        i.value = 'E4'
+    }
+    if (e.keyCode == 219) {
+        e3m.play()
+        i.value = 'E3'
+    }
+    if (e.keyCode == 49) {
+        f3m.play()
+        i.value = 'F3'
+    }
+    if (e.keyCode == 50) {
+        b5m.play()
+        i.value = 'B5'
+    }
+    if (e.keyCode == 51) {
+        ch3m.play()
+        i.value = 'C#3'
+    }
+    if (e.keyCode == 52) {
+        a4m.play()
+        i.value = 'A#4'
+    }
+    if (e.keyCode == 53) {
+        a5m.play()
+        i.value = 'A5'
+    }
+    if (e.keyCode == 54) {
+        b3m.play()
+        i.value = 'B3'
+    }
+    if (e.keyCode == 55) {
+        b4m.play()
+        i.value = 'B#3'
+    }
+    if (e.keyCode == 56) {
+        ah4m.play()
+        i.value = 'A#4'
+    }
+    if (e.keyCode == 57) {
+        ah5m.play()
+        i.value = 'A#5'
+    }
+    if (e.keyCode == 48) {
+        a3m.play()
+        i.value = 'A3'
+    }
+
+})
+
